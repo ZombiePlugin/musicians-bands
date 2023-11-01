0ea799598f9c3e7b0af3ff8b74ec7e1f72e5e4e9
M <muzameelahmed1@gmail.com>
2023-11-01 14:16:54 +0000

    models completed

--- models/Band.js
+++ models/Band.js
@@ -1,8 +1,12 @@
-const {Sequelize, sequelize} = require('../db');
+const { DataTypes } = require("sequelize");
+const { Sequelize, sequelize } = require("../db");
 
 // TODO - define the Band model
-let Band;
+let Band = sequelize.define("Band", {
+  name: DataTypes.STRING,
+  genre: DataTypes.STRING,
+});
 
 module.exports = {
-    Band
+  Band,
 };
--- models/Song.js
+++ models/Song.js
@@ -1,8 +1,13 @@
-const {Sequelize, sequelize} = require('../db');
+const { DataTypes } = require("sequelize");
+const { Sequelize, sequelize } = require("../db");
 
 // TODO - define the Song model
-let Song;
+let Song = sequelize.define("Song", {
+  title: DataTypes.STRING,
+  year: DataTypes.INTEGER,
+  length: DataTypes.INTEGER,
+});
 
 module.exports = {
-    Song
+  Song,
 };
